8901bd0764b44e50d0626154998246d4ac9658e0
Noa Markowitz <noamarkowitz@gmail.com>
2023-07-07 11:24:35 +0300

    try

--- src/containers/App.js
+++ src/containers/App.js
@@ -38,7 +38,7 @@ class App extends Component {
       <h1>Loading...</h1>
     ) : (
       <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
+        <h1 className="f1">RoboFriendss</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <Scroll>
           <CardList robots={filteredRobots} />
